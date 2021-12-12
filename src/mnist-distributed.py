29df663d47536b4b530d248c0a8900e8eb7df3ee
ATCP <poppin.ke@gmail.com>
2021-12-12 05:55:17 -0800

    misc

--- src/mnist-distributed.py
+++ src/mnist-distributed.py
@@ -236,7 +236,7 @@ def train(args):
 
     torch.cuda.set_device(gpu)
     model.cuda(gpu)
-    batch_size = 25
+    batch_size = 5
     # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda(gpu)
     optimizer = torch.optim.SGD(model.parameters(), 1e-4)
